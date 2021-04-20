5b4ea48a367c8e9d84ac605bbb699b9712df9e90
tam <cherykim123@gmail.com>
2021-04-19 18:54:57 -0700

    Link soda, sardines and chips to vendingmachine

--- src/components/VendingMachine.js
+++ src/components/VendingMachine.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import vendingMachineImage from "../v3.jpg";
 import "../style/VendingMachine.css";
 class VendingMachine extends Component {
@@ -9,6 +10,9 @@ class VendingMachine extends Component {
         style={{ backgroundImage: `url(${vendingMachineImage})` }}
       >
         <h1>HELLO, I AM A VENDING MACHINE. WHAT WOULD YOU LIKE TO HAVE</h1>
+        <Link to="/soda">Soda</Link>
+        <Link to="/sardines">Sardines</Link>
+        <Link to="/chips">Chips</Link>
       </div>
     );
   }
