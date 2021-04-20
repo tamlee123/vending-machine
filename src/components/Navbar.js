e30496862b4b80a240b4687e0f847f166c615b64
tam <cherykim123@gmail.com>
2021-04-19 22:36:12 -0700

    style for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "../style/Navbar.css";
 class Narbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/chips">Chips</NavLink>
-        <NavLink to="/sardines">Sardines</NavLink>
-        <NavLink to="/soda">Soda</NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/chips">
+          Chips
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/sardines">
+          Sardines
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/soda">
+          Soda
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/">
+          Home
+        </NavLink>
       </div>
     );
   }
