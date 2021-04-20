03edbb42b1ee7266156748758b9ecacd1588eb67
tam <cherykim123@gmail.com>
2021-04-19 18:28:48 -0700

    defining routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import VendingMachine from "./VendingMachine";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import Chips from "./Chips";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" component={VendingMachine} />
+        <Switch>
+          <Route path="/soda" component={Soda} />
+          <Route path="/sardines" component={Sardines} />
+          <Route path="/chips" component={Chips} />
+          <Route path="/" component={VendingMachine} exact />
+        </Switch>
       </div>
     );
   }
