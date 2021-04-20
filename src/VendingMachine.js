08563bfbd6553261beb2c3457409c01682b7acd4
tam <cherykim123@gmail.com>
2021-04-19 17:45:00 -0700

    first commit

---
+++ src/VendingMachine.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import vendingMachineImage from "./v3.jpg";
+import "./style/VendingMachine.css";
+class VendingMachine extends Component {
+  render() {
+    return (
+      <div
+        className="VendingMachine"
+        style={{ backgroundImage: `url(${vendingMachineImage})` }}
+      >
+        <h1>HELLO, I AM A VENDING MACHINE. WHAT WOULD YOU LIKE TO HAVE</h1>
+      </div>
+    );
+  }
+}
+
+export default VendingMachine;
